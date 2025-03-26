51839a50fbfc38502ec3c6beded342bb12681a8c
Viktoriia <lyudkevych.v@gmail.com>
2025-03-26 14:58:19 -0700

    Created wave3 part

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -41,11 +41,57 @@ def watch_movie(user_data, title):
 
 
 
-
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    #the list of movies the user has watched
+    user_watched = user_data["watched"]
 
+    # List that friend is watched(titles)
+    friends_watched = []
+
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friends_watched.append(movie["title"])
+    #store the movies that only the user has watched
+    unique_movies = []
+
+    for movie in user_watched:
+        if movie["title"] not in friends_watched:
+        #checking if the movie is not in friends' watched list, add to result
+            unique_movies.append(movie)
+    #list that user watched
+    return unique_movies
+
+#2
+def get_friends_unique_watched(user_data):
+    # list of movies the user has watched
+    user_watched = user_data["watched"]
+    # Get the list of friends
+    friends = user_data["friends"]
+
+    user_titles = []
+    for movie in user_watched:
+        user_titles.append(movie["title"])
+    
+    #Store unique movies friends have watched
+    friends_unique = []
+
+    seen_titles = set()
+
+    for friend in friends:
+        for movie in friend["watched"]:
+            title = movie["title"]
+
+            # If the user hasn't watched it and it's not already added
+            if title not in user_titles and title not in seen_titles:
+                friends_unique.append(movie)
+                # Title that seen to avoid duplicates
+                seen_titles.add(title)
+
+    #list that friends watched
+    return friends_unique
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
